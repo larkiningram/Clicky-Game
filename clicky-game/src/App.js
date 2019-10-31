6541fed0e303309253846da45230b58afcf47f85
Larkin <larkiningram@gmail.com>
2019-10-31 16:16:49 -0500

    game shuffles

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -3,6 +3,10 @@ import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import friends from "./friends.json";
 import "./App.css";
+// import Navbar from "./components/Navbar";
+
+let currentScore = 0;
+let highScore = 0;
 
 class App extends React.Component {
 
@@ -12,17 +16,48 @@ class App extends React.Component {
 
 
   clicker = (id) => {
-    const friends = this.state.friends.map((friend) => {
-      
+    const friends = this.state.friends.map(friend => {
+      if (friend.id === id && friend.clicked === false) {
+        friend.clicked = true;
+        currentScore++
+        console.log(friend)
+      }
+      else if (friend.id === id && friend.clicked === true) {
+        alert("you wrong")
+        currentScore = 0;
+      }
+      return friend
     })
     console.log("clicked")
+    console.log(friends)
     this.setState({ friends })
   }
+
+  shuffle = id => {
+    this.setState({
+        friends: this.state.friends.sort(function(a,b){
+                return 0.5 - Math.random();
+            }
+        )
+    })
+}
 
   render() {
     return (
       <Wrapper>
-        <h1 className="title">Friends List</h1>
+        <div className="row">
+          <nav className="navbar">
+            <div className="col-md-4">
+              <a href="/">Clicky Game</a>
+            </div>
+            <div className="col-md-4">
+              <p>Click on an image to play</p>
+            </div>
+            <div className="col-md-4">
+              <p>score: {currentScore} | high score: {highScore}</p>
+            </div>
+          </nav>
+        </div>
         {
           this.state.friends.map((friend) => {
             return (
@@ -32,6 +67,7 @@ class App extends React.Component {
                 image={friend.image}
                 click={this.clicker}
                 clicked={friend.clicked}
+                shuffle={this.shufflec}
                 id={friend.id}
               />
             )
--- clicky-game/src/components/FriendCard/index.js
+++ clicky-game/src/components/FriendCard/index.js
@@ -4,13 +4,13 @@ import "./style.css";
 
 
 function FriendCard(props) {
-  console.log(props)
+  // console.log(props)
   return (
     <div className="card">
       <div className="img-container">
         <img alt={props.name} src={props.image} />
       </div>
-      <span className="remove" onClick={() => props.click(props.id)}>𝘅</span>
+      <span className="remove" onClick={() => {props.click(props.id); props.shuffle(props.id)}}>𝘅</span>
     </div>
   );
 }
